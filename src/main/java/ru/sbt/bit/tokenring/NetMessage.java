c79d782fc278683babc826f626aa2740fe4c63e5
Dmitriy Ovchinnikov <ovchinnikov.d94@gmail.com>
2017-11-13 22:07:50 +0300

    Remove useless NetMessage constructors

--- src/main/java/ru/sbt/bit/tokenring/NetMessage.java
+++ src/main/java/ru/sbt/bit/tokenring/NetMessage.java
@@ -4,16 +4,7 @@ package ru.sbt.bit.tokenring;
 public class NetMessage {
     private int ttl = 128;
     private String msg;
-
-    public NetMessage(){
-        this.msg = "bitbitbitbit";
-        this.ttl = 128;
-    }
-
-    public NetMessage(int ttl) {
-        this();
-        this.ttl = ttl;
-    }
+    
 
     public NetMessage(String message, int ttl) {
         this.msg = message;
